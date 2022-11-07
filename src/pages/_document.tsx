400d8a132e038519f567af74ed9665ae3c810e50
Talisson Oliveira <talissonlux@outlook.com>
2022-11-07 00:47:08 -0300

    feat: adds page title and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <title>Home | Bolão da Copa 2022</title>
       </Head>
       <body className="bg-gray-900 bg-app bg-cover bg-no-repeat">
         <Main />
